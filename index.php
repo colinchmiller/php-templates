e509b370cce78941861a1556693f20afa40307f5
colinmiller <colinchmiller@gmail.com>
2016-03-22 09:40:22 -0500

    create variables in php to allow different pages in templates with

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
   include("templates/header.htm");
-  include("templates/index.htm");
+
+  $action = $_GET['action'];
+  // sanatise the data to ensure that a user cannot change directories at will
+  $action = basename($action);
+  include("templates/$action.htm");
   include("templates/footer.htm");
  ?>
