df8a7f744f42acedd7913559661cb5736ed3079b
colinmiller <colinchmiller@gmail.com>
2016-03-22 09:52:52 -0500

    error proofing for nonexistant pages and navigating to header or footer

--- index.php
+++ index.php
@@ -1,11 +1,17 @@
 <?php
   include("templates/header.htm");
 
-  if (!empty($_GET['action'])){
-    $action = $_GET['action'];
+  if (!empty($_GET["action"])){
+    $action = $_GET["action"];
     // sanatise the data to ensure that a user cannot change directories at will
     // removes the filename from the directory in the URL
     $action = basename($action);
+    //if the file doesn't exist, it will land the user on the home page
+    if (!file_exists("templates/$action.htm")){
+      $action = "index";
+    } elseif ($action == "header" || $action == "footer"){
+      $action = "index";
+    }
     include("templates/$action.htm");
   } else {
     include("templates/index.htm");
