04c002d7ee6d13be591ea063f2a9398e6bc2eca3
colinmiller <colinchmiller@gmail.com>
2016-03-22 09:45:36 -0500

    add landing if statement so that '?action=index' is not needed to land on main page

--- index.php
+++ index.php
@@ -1,9 +1,14 @@
 <?php
   include("templates/header.htm");
 
-  $action = $_GET['action'];
-  // sanatise the data to ensure that a user cannot change directories at will
-  $action = basename($action);
-  include("templates/$action.htm");
+  if (!empty($_GET['action'])){
+    $action = $_GET['action'];
+    // sanatise the data to ensure that a user cannot change directories at will
+    // removes the filename from the directory in the URL
+    $action = basename($action);
+    include("templates/$action.htm");
+  } else {
+    include("templates/index.htm");
+  }
   include("templates/footer.htm");
  ?>
